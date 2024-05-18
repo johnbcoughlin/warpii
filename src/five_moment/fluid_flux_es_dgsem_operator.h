206d30d7852c70b6f2e9782258ae9ba13aa1224b
Jack Coughlin <jack@johnbcoughlin.com>
2024-05-17 22:57:29 -0700

    WIP broken EC flux function

--- src/five_moment/fluid_flux_es_dgsem_operator.h
+++ src/five_moment/fluid_flux_es_dgsem_operator.h
@@ -3,12 +3,14 @@
 #include <deal.II/base/array_view.h>
 #include <deal.II/base/quadrature_lib.h>
 #include <deal.II/base/utilities.h>
+#include <deal.II/fe/fe_series.h>
 #include <deal.II/fe/fe_update_flags.h>
 #include <deal.II/fe/fe_values_extractors.h>
 #include <deal.II/hp/q_collection.h>
 #include <deal.II/lac/vector.h>
 #include <deal.II/matrix_free/matrix_free.h>
-#include <deal.II/fe/fe_series.h>
+
+#include <ios>
 
 #include "../dof_utils.h"
 #include "dg_discretization.h"
@@ -51,9 +53,8 @@ class FluidFluxESDGSEMOperator {
         const double dt, const double t, const double alpha = 1.0,
         const double beta = 0.0) const;
 
-    double recommend_dt(
-        const MatrixFree<dim, double> &mf,
-            const LinearAlgebra::distributed::Vector<double> &sol);
+    double recommend_dt(const MatrixFree<dim, double> &mf,
+                        const LinearAlgebra::distributed::Vector<double> &sol);
 
    private:
     void local_apply_inverse_mass_matrix(
@@ -90,10 +91,25 @@ class FluidFluxESDGSEMOperator {
      *
      * @param phi: Should have already been reinited for the current cell
      */
-    std::array<double, dim> shock_indicators(
-        const FEEvaluation<dim, -1, 0, dim+2, double>& phi,
-        const FESeries::Legendre<dim> &legendre
-            ) const;
+    std::array<VectorizedArray<double>, dim> shock_indicators(
+        const FEEvaluation<dim, -1, 0, dim + 2, double> &phi,
+        FESeries::Legendre<dim> &legendre) const;
+
+    void calculate_high_order_EC_flux(
+        LinearAlgebra::distributed::Vector<double> &dst,
+        FEEvaluation<dim, -1, 0, dim + 2, double>& phi,
+        const FEEvaluation<dim, -1, 0, dim + 2, double>& phi_reader,
+        const FullMatrix<double>& D, unsigned int d, 
+        VectorizedArray<double> alpha,
+        bool log=false) const;
+
+    void calculate_first_order_ES_flux(
+        LinearAlgebra::distributed::Vector<double> &dst,
+        FEEvaluation<dim, -1, 0, dim + 2, double>& phi,
+        const FEEvaluation<dim, -1, 0, dim + 2, double>& phi_reader,
+        const std::vector<double> &quadrature_weights, 
+        const FullMatrix<double>& Q,
+        unsigned int d, VectorizedArray<double> alpha) const;
 
     std::shared_ptr<FiveMomentDGDiscretization<dim>> discretization;
     double gas_gamma;
@@ -192,21 +208,25 @@ void FluidFluxESDGSEMOperator<dim>::local_apply_cell(
     FEValues<dim> fe_values(discretization->get_mapping(),
                             discretization->get_fe(), QGaussLobatto<dim>(Np),
                             UpdateFlags::update_values);
+    const std::vector<double> &quadrature_weights =
+        fe_values.get_quadrature().get_weights();
 
     std::vector<unsigned int> n_coefs_per_dim = {};
     for (unsigned int d = 0; d < dim; d++) {
         n_coefs_per_dim.push_back(Np);
     }
-    FESeries::Legendre<dim> legendre(n_coefs_per_dim,
-            hp::FECollection<dim>(fe_values.get_fe()),
-            hp::QCollection<dim>(fe_values.get_quadrature()), 
-            0);
+    auto fe_collection = hp::FECollection<dim>(fe_values.get_fe().base_element(0));
+    auto q_collection = hp::QCollection<dim>(fe_values.get_quadrature());
+    FESeries::Legendre<dim> legendre(
+        n_coefs_per_dim, fe_collection, q_collection, 0);
 
     FullMatrix<double> D(Np, Np);
+    FullMatrix<double> Q(Np, Np);
     for (unsigned int j = 0; j < Np; j++) {
         for (unsigned int l = 0; l < Np; l++) {
             Point<dim> j_pt = fe_values.get_quadrature().point(j);
             D(j, l) = fe_values.get_fe().shape_grad(l, j_pt)[0];
+            Q(j, l) = quadrature_weights[j] * D(j, l);
         }
     }
 
@@ -223,54 +243,189 @@ void FluidFluxESDGSEMOperator<dim>::local_apply_cell(
             phi.reinit(cell);
             phi_reader.reinit(cell);
             phi_reader.gather_evaluate(src, EvaluationFlags::values);
+            //phi_reader.read_dof_values(src);
+
+            //std::cout << "q0: " << phi_reader.get_value(0) << std::endl;
+            //const std::array<VectorizedArray<double>, dim> alphas = shock_indicators(phi_reader, legendre);
 
             for (unsigned int d = 0; d < dim; d++) {
-                Tensor<1, dim, VectorizedArray<double>> unit_basis_vec;
-                for (unsigned int di = 0; di < dim; di++) {
-                    unit_basis_vec[di] = VectorizedArray((di == d) ? 1.0 : 0.0);
-                }
+                //VectorizedArray<double> alpha = alphas[d];
+                VectorizedArray<double> alpha = VectorizedArray<double>(0.0);
+                bool log = (cell >= 49 && cell <= 51);
+                calculate_high_order_EC_flux(dst, phi, phi_reader, D, d, alpha, log);
 
-                for (const unsigned int qj : phi.quadrature_point_indices()) {
-                    Tensor<2, dim, VectorizedArray<double>> Jinv_j =
-                        phi.inverse_jacobian(qj);
-                    VectorizedArray<double> Jdet_j =
-                        1.0 / warpii::determinant(Jinv_j);
-
-                    auto Jai_j = Jdet_j * tensor_column(Jinv_j, d);
-
-                    unsigned int j = quad_point_1d_index(qj, Np, d);
-                    auto uj = phi_reader.get_value(qj);
-                    Tensor<1, dim + 2, VectorizedArray<double>> flux_j;
-                    for (unsigned int di = 0; di < dim; di++) {
-                        flux_j[di] = 0.0;
-                    }
-
-                    for (unsigned int l = 0; l < Np; l++) {
-                        unsigned int ql =
-                            quadrature_point_neighbor(qj, l, Np, d);
-                        Tensor<2, dim, VectorizedArray<double>> Jinv_l =
-                            phi.inverse_jacobian(ql);
-                        VectorizedArray<double> Jdet_l =
-                            1.0 / warpii::determinant(Jinv_l);
-                        auto Jai_l = Jdet_l * tensor_column(Jinv_j, d);
-
-                        const auto Jai_avg = 0.5 * (Jai_j + Jai_l);
-
-                        auto ul = phi_reader.get_value(ql);
-                        double d_jl = D(j, l);
-                        auto two_pt_flux =
-                            euler_CH_EC_flux<dim>(uj, ul, gas_gamma);
-                        flux_j -= 2.0 * d_jl * two_pt_flux * Jai_avg;
-                    }
-                    phi.submit_value(flux_j / Jdet_j, qj);
-                }
-
-                // Need to be careful to integrate after each flux dimension d,
-                // otherwise the quadrature point values get overwritten.
-                phi.integrate_scatter(EvaluationFlags::values, dst);
+                //calculate_first_order_ES_flux(
+                 //   dst, phi, phi_reader,
+                   // fe_values.get_quadrature().get_weights(), Q, d, alpha);
             }
         }
     }
+}
+
+template <int dim>
+VectorizedArray<double> jacobian_determinant(
+    FEEvaluation<dim, -1, 0, dim + 2, double> phi, unsigned int q) {
+    Tensor<2, dim, VectorizedArray<double>> Jinv_j = phi.inverse_jacobian(q);
+    VectorizedArray<double> Jdet_j = 1.0 / warpii::determinant(Jinv_j);
+    return Jdet_j;
+}
+
+/**
+ * Computes the contravariant basis vector Ja^d at the quadrature point q,
+ * where d is the dimension index.
+ *
+ * For details on what we're doing here, see Winters et al. (2020),
+ * "Construction of Modern Robust Nodal Discontinuous Galerkin Spectral Element
+ * Methods for the Compressible Navier-Stokes Equations", Section 4.3
+ *
+ * This is equation (172), taking advantage of the fact that
+ * `FEEvaluation::inverse_jacobian` returns precisely the matrix whose columns
+ * are \vec{a}^i. We then multiply by the Jacobian determinant J.
+ */
+template <int dim>
+Tensor<1, dim, VectorizedArray<double>> scaled_contravariant_basis_vector(
+    FEEvaluation<dim, -1, 0, dim + 2, double> phi, unsigned int q,
+    unsigned int d) {
+    Tensor<2, dim, VectorizedArray<double>> Jinv_j = phi.inverse_jacobian(q);
+    VectorizedArray<double> Jdet_j = jacobian_determinant(phi, q);
+
+    return Jdet_j * tensor_column(Jinv_j, d);
+}
+
+/**
+ * This function is based on the documentation for the ordering of FE_DGQ
+ * DOFs, found here:
+ * https://www.dealii.org/current/doxygen/deal.II/classFE__DGQ.html
+ */
+template <int dim>
+std::vector<unsigned int> pencil_starts(unsigned int, unsigned int d) {
+    AssertThrow(d < dim,
+                ExcMessage("Specified dimension d must be less than n_dims."));
+    if (dim == 1) {
+        return {0};
+    } else {
+        AssertThrow(false, ExcMessage("Unimplemented"));
+    }
+}
+
+/**
+ * This function is based on the documentation for the ordering of FE_DGQ
+ * DOFs, found here:
+ * https://www.dealii.org/current/doxygen/deal.II/classFE__DGQ.html
+ */
+template <int dim>
+unsigned int pencil_stride(unsigned int Np, unsigned int d) {
+    AssertThrow(d < dim,
+                ExcMessage("Specified dimension d must be less than n_dims."));
+    if (d == 0) {
+        return 1;
+    } else if (d == 1) {
+        return Np;
+    } else if (d == 2) {
+        return Np * Np;
+    } else {
+        AssertThrow(false, ExcMessage("n_dims appears to be 4 or more"));
+    }
+}
+
+template <int dim>
+void FluidFluxESDGSEMOperator<dim>::calculate_high_order_EC_flux(
+    LinearAlgebra::distributed::Vector<double> &dst,
+    FEEvaluation<dim, -1, 0, dim + 2, double>& phi,
+    const FEEvaluation<dim, -1, 0, dim + 2, double>& phi_reader,
+    const FullMatrix<double>& D, 
+    unsigned int d, VectorizedArray<double> alpha,
+    bool log) const {
+    unsigned int fe_degree = discretization->get_fe_degree();
+    unsigned int Np = fe_degree + 1;
+
+    for (const unsigned int qj : phi.quadrature_point_indices()) {
+        auto Jdet_j = jacobian_determinant(phi, qj);
+        auto Jai_j = scaled_contravariant_basis_vector(phi, qj, d);
+
+        unsigned int j = quad_point_1d_index(qj, Np, d);
+        auto uj = phi_reader.get_value(qj);
+        Tensor<1, dim + 2, VectorizedArray<double>> flux_j;
+        for (unsigned int di = 0; di < dim; di++) {
+            flux_j[di] = 0.0;
+        }
+
+        for (unsigned int l = 0; l < Np; l++) {
+            unsigned int ql = quadrature_point_neighbor(qj, l, Np, d);
+            auto Jai_l = scaled_contravariant_basis_vector(phi, ql, d);
+
+            const auto Jai_avg = 0.5 * (Jai_j + Jai_l);
+
+            auto ul = phi_reader.get_value(ql);
+            double d_jl = D(j, l);
+            if (log) {
+                std::cout << "Jai_j = " << Jai_j << std::endl;
+                std::cout << "Jai_l = " << Jai_l << std::endl;
+                std::cout << "ul = " << ul << std::endl;
+                std::cout << "uj = " << uj << std::endl;
+                std::cout << "d_jl = " << d_jl << std::endl;
+            }
+            auto two_pt_flux = euler_CH_EC_flux<dim>(uj, ul, gas_gamma);
+            flux_j -= 2.0 * d_jl * two_pt_flux * Jai_avg;
+        }
+        //std::cout << "alpha = " << alpha << std::endl;
+        //std::cout << "flux_j = " << flux_j << std::endl;
+        //std::cout << "Jdet_j = " << Jdet_j << std::endl;
+        phi.submit_value((1.0 - alpha) * flux_j / Jdet_j, qj);
+    }
+
+    // Need to be careful to integrate for each flux dimension d,
+    // otherwise the quadrature point values get overwritten for the next
+    // dimension.
+    phi.integrate_scatter(EvaluationFlags::values, dst);
+}
+
+template <int dim>
+void FluidFluxESDGSEMOperator<dim>::calculate_first_order_ES_flux(
+    LinearAlgebra::distributed::Vector<double> &dst,
+    FEEvaluation<dim, -1, 0, dim + 2, double>& phi,
+    const FEEvaluation<dim, -1, 0, dim + 2, double>& phi_reader,
+    const std::vector<double> &quadrature_weights, 
+    const FullMatrix<double> &Q,
+    unsigned int d, VectorizedArray<double> alpha) const {
+    unsigned int fe_degree = discretization->get_fe_degree();
+    unsigned int Np = fe_degree + 1;
+
+    unsigned int stride = pencil_stride<dim>(Np, d);
+    for (unsigned int pencil_start : pencil_starts<dim>(Np, d)) {
+        Tensor<1, dim, VectorizedArray<double>> n_i_iplus1 =
+            scaled_contravariant_basis_vector(phi, pencil_start, d);
+
+        // Index i runs over subcells, and at each subcell we consider the right
+        // face. So should skip the final subcell whose right face is handled by
+        // the usual numerical flux.
+        for (unsigned int i = 0; i < Np - 1; i++) {
+            unsigned int qi = pencil_start + stride * i;
+
+            for (unsigned int m = 0; m < Np; m++) {
+                unsigned int qm = pencil_start + stride * m;
+                Tensor<1, dim, VectorizedArray<double>> Jad_m =
+                    scaled_contravariant_basis_vector(phi, qm, d);
+                n_i_iplus1 += Q(i, m) * Jad_m;
+            }
+
+            // We're going to calculate the numerical flux across the subcell
+            // face from subcell i to subcell i+1.
+            const auto left_state = phi_reader.get_value(qi);
+            const auto right_state = phi_reader.get_value(qi + stride);
+            const auto flux = euler_CH_entropy_dissipating_flux<dim>(
+                left_state, right_state, gas_gamma);
+            const auto flux_dot_n = flux * n_i_iplus1;
+
+            // Perform a face-centered flux difference, so subtract from the
+            // left subcell and add to the right subcell. Equation (13) in
+            // Henneman et al.
+            phi.submit_value(-alpha * flux_dot_n / quadrature_weights[i], qi);
+            phi.submit_value(alpha * flux_dot_n / quadrature_weights[i + 1],
+                             qi + stride);
+        }
+    }
+    phi.integrate_scatter(EvaluationFlags::values, dst);
 }
 
 template <int dim>
@@ -301,9 +456,10 @@ void FluidFluxESDGSEMOperator<dim>::local_apply_face(
                     euler_flux<dim>(phi_m.get_value(q), gas_gamma) * n;
                 const auto flux_p =
                     euler_flux<dim>(phi_p.get_value(q), gas_gamma) * n;
-                const auto numerical_flux = euler_CH_entropy_dissipating_flux<dim>(
-                    phi_p.get_value(q), phi_m.get_value(q),
-                    gas_gamma) * phi_m.get_normal_vector(q);
+                const auto numerical_flux =
+                    euler_CH_entropy_dissipating_flux<dim>(
+                        phi_p.get_value(q), phi_m.get_value(q), gas_gamma) *
+                    phi_m.get_normal_vector(q);
 
                 phi_m.submit_value(flux_m - numerical_flux, q);
                 phi_p.submit_value(numerical_flux - flux_p, q);
@@ -399,11 +555,12 @@ void FluidFluxESDGSEMOperator<dim>::local_apply_boundary_face(
 
 template <int dim>
 double FluidFluxESDGSEMOperator<dim>::recommend_dt(
-        const MatrixFree<dim, double> &mf,
+    const MatrixFree<dim, double> &mf,
     const LinearAlgebra::distributed::Vector<double> &sol) {
     double max_transport_speed = compute_cell_transport_speed(mf, sol);
     unsigned int fe_degree = discretization->get_fe_degree();
-    std::cout << (max_transport_speed * (fe_degree+1) * (fe_degree+1)) << std::endl;
+    std::cout << (max_transport_speed * (fe_degree + 1) * (fe_degree + 1))
+              << std::endl;
     return 1.0 / (max_transport_speed * (fe_degree + 1) * (fe_degree + 1));
 }
 
@@ -474,27 +631,89 @@ double FluidFluxESDGSEMOperator<dim>::compute_cell_transport_speed(
 }
 
 template <int dim>
-std::array<double, dim> FluidFluxESDGSEMOperator<dim>::shock_indicators(
-        const FEEvaluation<dim, -1, 0, dim+2, double> &phi,
-        const FESeries::Legendre<dim> &legendre) {
+std::array<VectorizedArray<double>, dim> FluidFluxESDGSEMOperator<dim>::shock_indicators(
+    const FEEvaluation<dim, -1, 0, dim + 2, double> &phi,
+    FESeries::Legendre<dim> &legendre) const {
+    unsigned int Np = discretization->get_fe_degree() + 1;
+    AssertThrow(Np >= 2, ExcMessage("Shock indicators are not supported nor "
+                                    "needed for P0 polynomial bases"));
+
+    std::array<VectorizedArray<double>, dim> alphas;
+
     Vector<double> p_times_rho;
     p_times_rho.reinit(phi.dofs_per_component);
+    for (unsigned int lane = 0; lane < VectorizedArray<double>::size(); lane++) {
+        for (unsigned int dof = 0; dof < phi.dofs_per_component; dof++) {
+            const auto q_dof = phi.get_dof_value(dof);
+            std::cout << "q_dof = " << q_dof << std::endl;
+            const auto rho = q_dof[0][lane];
+            const auto p = euler_pressure<dim>(q_dof, gas_gamma)[lane];
+            p_times_rho(dof) = p * rho;
+        }
 
-    for (unsigned int dof = 0; dof < phi.dofs_per_component; dof++) {
-        const auto q_dof = phi.get_dof_value(dof);
-        const auto rho = q_dof[0];
-        const auto p = euler_pressure<dim>(q_dof);
-        p_times_rho(dof) = p * rho;
+        TableIndices<dim> sizes;
+        for (unsigned int d = 0; d < dim; d++) {
+            sizes[d] = Np;
+        }
+        Table<dim, double> legendre_coefs;
+        legendre_coefs.reinit(sizes);
+        legendre.calculate(p_times_rho, 0, legendre_coefs);
+
+        for (unsigned int d = 0; d < dim; d++) {
+            const std::function<std::pair<bool, unsigned int>(
+                const TableIndices<dim> &index)>
+                group_leading_coefs = [&d, &Np](const TableIndices<dim> &index)
+                -> std::pair<bool, unsigned int> {
+                std::size_t i = index[d];
+                if (i < Np - 1) {
+                    return std::make_pair(true, 0);
+                } else if (i == Np - 1) {
+                    return std::make_pair(true, 1);
+                } else if (i == Np) {
+                    return std::make_pair(true, 2);
+                } else {
+                    return std::make_pair(false, 3);
+                }
+            };
+            const std::pair<std::vector<unsigned int>, std::vector<double>>
+                grouped_coefs = FESeries::process_coefficients(
+                    legendre_coefs, group_leading_coefs,
+                    VectorTools::NormType::L2_norm);
+            double total_energy = 0.;
+            double total_energy_minus_1 = 0.;
+            double top_mode = 0.;
+            double top_mode_minus_1 = 0.;
+            for (unsigned int i = 0; i < grouped_coefs.first.size(); i++) {
+                unsigned int predicate = grouped_coefs.first[i];
+                double sqrt_energy = grouped_coefs.second[i];
+                double energy = sqrt_energy * sqrt_energy;
+                if (predicate == 0) {
+                    total_energy += energy;
+                    total_energy_minus_1 += energy;
+                } else if (predicate == 1) {
+                    top_mode_minus_1 += energy;
+                    total_energy_minus_1 += energy;
+                    total_energy += energy;
+                } else if (predicate == 2) {
+                    top_mode += energy;
+                    total_energy += energy;
+                }
+            }
+            double E = std::max(top_mode / total_energy,
+                                top_mode_minus_1 / total_energy_minus_1);
+            double T = 0.5 * std::pow(10.0, -1.8 * std::pow(Np, 0.25));
+            double s = 9.21024;
+            double alpha = 1.0 / (1.0 + std::exp(-s / T * (E - T)));
+
+            if (alpha < 1e-3) {
+                alpha = 0.0;
+            } else if (alpha > 0.5) {
+                alpha = 0.5;
+            }
+            alphas[d][lane] = alpha;
+        }
     }
-
-    Table<dim, double> legendre_coefs;
-    legendre.calculate(p_times_rho, 0, legendre_coefs); 
-
-    const auto group_leading_coefs
-
-    for (unsigned int d = 0; d < dim; d++) {
-
-    }
+    return alphas;
 }
 
 }  // namespace five_moment
